50ab364616f2f4eccb90bbdbe48a8ae052221c52
ludiemert <lucianadiemert@gmail.com>
2025-04-16 21:10:43 +0100

    add comands os.path

---
+++ Aulas/3 - Modulo OS - Automacao basica com OS/Atividades_OS/os_path.py
@@ -0,0 +1,12 @@
+# as funcoes agora ficam dentro de os.path
+# testes de funcoes do os.path
+
+import os
+
+# %%
+# caminho onde estou
+os.getcwd()
+#'c:\\Users\\user\\Downloads\\Computer_Vision_Py\\Aulas\\3 - Modulo OS - Automacao basica com OS\\Atividades_OS'
+
+# %%
+os.path.
