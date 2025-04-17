470dc528f8d2d92b12a4bca49ea684144f5876cc
ludiemert <lucianadiemert@gmail.com>
2025-04-17 16:27:56 +0100

    add file projet exec

---
+++ Aulas/3_Modulo_OS_Automacao_basica_OS/Atividades_OS/Exerc/in.py
@@ -0,0 +1,9 @@
+# criar um script para colocar todos os arquivos em pasta ordenadas em pastas
+
+import os
+
+# %%
+cwd = os.getcwd()  # obtém o diretório atual
+print("Diretório atual:", cwd)  # exibe o diretório atual
+
+# %%
