094135898248b3b60bed925998bd7d1eb9b210a2
ludiemert <lucianadiemert@gmail.com>
2025-04-18 13:24:53 +0100

    add file projet exec # se tem substring dentro dela '.py' not in i => 'text' in 'teste' # False porque nao esta dentro de text

--- Aulas/3_Modulo_OS_Automacao_basica_OS/Atividades_OS/Exerc/in.py
+++ Aulas/3_Modulo_OS_Automacao_basica_OS/Atividades_OS/Exerc/in.py
@@ -7,3 +7,15 @@ cwd = os.getcwd()  # obtém o diretório atual
 print("Diretório atual:", cwd)  # exibe o diretório atual
 
 # %%
+# full lista (full_list variavel) de todos arquivos dentro dessa pasta
+full_list = os.listdir(cwd)
+print("Full List file:", full_list)
+# %%
+# tb separar os arq py, usando copreesao em lista
+# que seja somente arquivos e que os arquivos .py nao esteja presente nessa string de codigo
+# se tem substring dentro dela '.py' not in i => 'text' in 'teste' # False porque nao esta dentro de text
+files_list = [i for i in full_list if os.path.isfile(i) and '.py' not in i]
+print("files_list: ", files_list)
+
+
+# %%
