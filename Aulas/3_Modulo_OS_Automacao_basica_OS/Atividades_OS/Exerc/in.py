e48af13ff24218c3174c1458679c18089b7b45b7
ludiemert <lucianadiemert@gmail.com>
2025-04-18 13:51:52 +0100

    add cod preciso saber os tipos na lista final, pegando somente a extensao

--- Aulas/3_Modulo_OS_Automacao_basica_OS/Atividades_OS/Exerc/in.py
+++ Aulas/3_Modulo_OS_Automacao_basica_OS/Atividades_OS/Exerc/in.py
@@ -13,9 +13,18 @@ print("Full List file:", full_list)
 # %%
 # tb separar os arq py, usando copreesao em lista
 # que seja somente arquivos e que os arquivos .py nao esteja presente nessa string de codigo
-# se tem substring dentro dela '.py' not in i => 'text' in 'teste' # False porque nao esta dentro de text
-files_list = [i for i in full_list if os.path.isfile(i) and '.py' not in i]
-print("files_list: ", files_list)
+# se tem substring dentro dela '.py' not in i => 'text' in 'teste' # False porque nao esta dentro de text, USANDO TUPLE  and not i.endswith(('.py', '.ipynb'))]
+# se fosse so file py o comando seria: files_list = [i for i in full_list if os.path.isfile(i) and '.py' not in i]
+files_list = [i for i in full_list if os.path.isfile(i) and not i.endswith(('.py', '.ipynb'))]
+#print("files_list: ", files_list)
+
+# %%
+# preciso saber os tipos na lista final, pegando somente a extensao
+types = [i.split('.')[1] for i in files_list]
+# essa logica types = [i for i in files_list]
+#'teste09.docx'.split('.')[1]  #'docx' pega o segundo elemento
+#'teste09.docx'.split('.')  # ['teste09', 'docx']
+print("types: ", types)
 
 
 # %%
