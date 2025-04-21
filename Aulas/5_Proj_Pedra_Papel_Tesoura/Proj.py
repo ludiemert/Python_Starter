0e67145f25ddd49e318d79e505cc8b192cb3139d
ludiemert <lucianadiemert@gmail.com>
2025-04-22 00:19:20 +0100

    start codig proj Pedra papel tesoura

---
+++ Aulas/5_Proj_Pedra_Papel_Tesoura/Proj.py
@@ -0,0 +1,22 @@
+# projeto pedra, papel e tesoura
+
+#%%
+import random
+import os
+
+# crirar uma lista com as 3 jogadas
+mover = ["papel", "pedra", "tesoura"]
+
+# uma variavel para segurar o placar do jogador e do computador
+player_count = 0
+computer_count = 0
+
+print("======================")
+print("Welcome to the Game 🥰")
+
+# funcao para sempre ficar o print na tela
+def main_print():
+  print("====================")
+  print("\nPLACAR")
+
+# %%
