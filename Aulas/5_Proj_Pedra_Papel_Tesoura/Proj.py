c27a0a31611a157c65c818daf148929a629b9eba
ludiemert <lucianadiemert@gmail.com>
2025-04-23 21:46:12 +0100

    # add function

--- Aulas/5_Proj_Pedra_Papel_Tesoura/Proj.py
+++ Aulas/5_Proj_Pedra_Papel_Tesoura/Proj.py
@@ -4,7 +4,7 @@ import random
 import os
 
 # crirar uma lista com as 3 jogadas
-mover = ["Paper", "Stones", "Scissors"]
+move_list = ["Paper", "Stones", "Scissors"]
 
 # uma variavel para segurar o placar do jogador e do computador
 player_count = 0
@@ -23,4 +23,23 @@ def main_print():
     print("\n")
     print("Choose Your Bid: ")
     print("0 - Paper | 1 - Stones | 2 - Scissors")
+
+    # funcao que escolhe um lance do computador
+    def select_move(): # nome da funcao
+        return random.choice(move_list) # escolhe um elemento da lista de forma aleatoria, pega o move_list e retorna um elemento dessa lista aleatoriamente
+
+    # prox funcao que recebe a jogada do jogador (nos)
+    def get_player_move():
+        while True: # so eh possivel sair desse while tru se digitar oque eh esperado ()
+            try: # proteje o codigo com try
+                player_move = int(input()) # converte para inteiro
+                if player_move not in[0, 1, 2]: # pergunta se esta dentro da lista que a gente quer, 0,1,2
+                    raise # sem ele nao estiver retorna um erro, proteje o programa
+                return move_list[player_move] # se ela digitar certo 0,1 ou 2 vem p ca
+            except Exception as e:
+                print(e)
+
+    # funcao que vai definir quem venceu
+
+
 
