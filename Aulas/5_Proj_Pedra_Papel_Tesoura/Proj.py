a1067b8d62d4ac21c2952d0fc82ebfbe8a2dedce
ludiemert <lucianadiemert@gmail.com>
2025-04-23 21:29:18 +0100

    # funcao que print valores and add img project start and requirements

--- Aulas/5_Proj_Pedra_Papel_Tesoura/Proj.py
+++ Aulas/5_Proj_Pedra_Papel_Tesoura/Proj.py
@@ -1,11 +1,10 @@
 # projeto pedra, papel e tesoura
 
-#%%
 import random
 import os
 
 # crirar uma lista com as 3 jogadas
-mover = ["papel", "pedra", "tesoura"]
+mover = ["Paper", "Stones", "Scissors"]
 
 # uma variavel para segurar o placar do jogador e do computador
 player_count = 0
@@ -15,8 +14,13 @@ print("======================")
 print("Welcome to the Game 🥰")
 
 # funcao para sempre ficar o print na tela
+# funcao que print valores
 def main_print():
-  print("====================")
-  print("\nPLACAR")
+    print("====================")
+    print("\nPLACAR")
+    print("You: {}".format(player_count))
+    print("Computer: {}".format(computer_count))
+    print("\n")
+    print("Choose Your Bid: ")
+    print("0 - Paper | 1 - Stones | 2 - Scissors")
 
-# %%
