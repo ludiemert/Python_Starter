465dccc8e4912a37baa5cca50c04ff01f14f04ef
ludiemert <lucianadiemert@gmail.com>
2025-06-08 12:11:19 +0100

    add new project simulado_caos_academia

---
+++ Projetos/7_Simulando_caos_academia/academia.py
@@ -0,0 +1,14 @@
+# academia organizada e desorganizada (halteres de academia)
+# usou o py 3.9.4 eu estou com 3.13
+# %%
+import random # posicoes aleatorias
+
+# %%
+#criar a classe e a funcao
+class Academia:
+  def __init__(self):
+      self.halteres = [i for i in range(10, 36) if i % 2 == 0]
+
+# dica para depurar classe
+self = Academia()
+# %%
